58eae031ac4ab4e8eac05002c90b72e20bb6dc62
Jose Amoreira <ljmamoreira@gmail.com>
2016-07-17 00:53:06 +0100

    Initial commit.

---
+++ init.py
@@ -0,0 +1,11 @@
+#File init.py
+#José Amoreira
+#July 2016
+
+import numpy
+
+def init(xc, xf):
+    """init(). Defines the values for density, velocity, conduction coefficient,
+       source terms and boundary values at each CV.
+    """
+    
---
+++ mesh.py
@@ -0,0 +1,17 @@
+#File mesh.py
+#José Amoreira
+#July 2016
+
+import numpy as np
+
+def mkmesh(n,a,b):
+    """mkmesh(a,b,n). Generates a uniform centered mesh, defined by np.arrays
+       xc[n] storing the center cell positions, and xf[n+1] storing the cell
+       faces positions.
+    """
+    assert isisntance(n,int), "mkmesh: n is not an integer."
+    xf = np.linspace(a, b, n+1)
+    xc = (xf[1:] + xf[:-1])/2.0
+    return xc, xf
+
+
