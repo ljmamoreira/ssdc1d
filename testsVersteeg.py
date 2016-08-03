18949f9ba0ba993573910dc9b16ec51dd204bb18
Jose Amoreira <ljmamoreira@gmail.com>
2016-08-03 01:57:21 +0100

    Refactored tests, it only tests V&M chpt 5 examples.

--- testsVersteeg.py
+++ testsVersteeg.py
@@ -21,7 +21,7 @@ def printArray(a,label=""):
     print label+" "+' '.join('{:7.4f}'.format(x) for x in a)
 
 #Define here a local version of init(), in order to keep the parameters
-def init(mesh):
+def init(mesh,velocity):
     """init(mesh). Defines the values for density, velocity, conduction
        coefficient, convection and diffusion strength parameters F and D
        source terms and boundary values at each CV.
@@ -29,11 +29,11 @@ def init(mesh):
     xc, xf = mesh
 
     #Values below taken from Versteeg & Malalasekera Ex. 5.1b.
-    #DO NOT CHANGE THE VALUES!
+    #DO NOT CHANGE THE VALUES! 
 
     #Density, velocity, convection strength parameter F:
-    rho = 1.5
-    v = 1.1
+    rho = 1.0
+    v = velocity
     F = np.ones_like(xf) * rho * v
 
     #Conduction coefficient and diffusion strength parameter D:
@@ -66,15 +66,36 @@ def isSmallDiff(a1,a2):
 
 def okIfTrue(ok):
     if ok:
-        return "OK."
-    return "wrong."
+        return "OK"
+    return "Wrong"
 
-def printReport(name,expected,computed):
-    isOK = isSmallDiff(expected, computed)
-    print name,okIfTrue(isOK)
-    if not isOK:
-        print "   Expected: ", expected
-        print "   Computed: ", computed
+
+def report_aPWEb(computed, versteeg):
+    aPc, aWc, aEc, bPc = computed
+    aPv, aWv, aEv, bPv = versteeg
+    print ("{:2s}  "+4*"{:^7s} {:^7s}  ").format('i', 'aPc', 'aPv', 'aWc',
+            'aWv', 'aEc', 'aEv', 'bPc', 'bPv')
+    for (i, (apc, apv, awc, awv, aec, aev, bc, bv)) in enumerate(zip(
+        aPc, aPv, aWc, aWv, aEc, aEv, bPc, bPv)):
+        print ('{: 2d}  '+4*'{:7.4f} {:7.4f}  ').format(
+            i+1, apc, apv, awc, awv, aec, aev, bc, bv)
+    sumUp ='    ' + '  '.join('{:^15s}'.format(okIfTrue(isSmallDiff(ac,av)))
+        for ac, av in zip(computed, versteeg))
+    print sumUp
+    return reduce(lambda u,v: u and v, [isSmallDiff(ac,av) for ac, av in
+        zip(computed, versteeg)])
+
+
+def report_y(xc, yc, yv, ya):
+    print
+    print ("{:^2s}  {:^3s}" + 3*"  {:^6s}").format('i', 'x', 'yc', 'yv', 'ya')
+    for i, (x, fc, fv, fa) in enumerate(zip(xc, yc, yv, ya)):
+        print ("{:2d}  {:3.1f}"+3*"  {:6.4f}").format(i,x,fc,fv,fa)
+    result = isSmallDiff(yc,yv)
+    print "Solution: ", okIfTrue(result)
+    return result
+    
+
 
 
 #Tests start here
@@ -88,94 +109,120 @@ def test_ucmesh():
     printReport("Mesh face coords", xfExpected, xf)
 
 
-def test_lds():
-    mesh = meshmaker.ucmesh(25, 0.0, 1.0)
-    FD, srcCoeffs, bdrVals, mu = init(mesh)
+
+#5.1(i): v=0.1, N=5, LDS
+def test_V51i():
+    print "\n"+30*"="
+    print "V&M, Example 5.1(i): LDS, N=5, v=0.1"
+    mesh = meshmaker.ucmesh(5, 0.0, 1.0)
+    FD, srcCoeffs, bdrVals, mu = init(mesh, velocity=0.1)
     stdFormCoeffs = discr.stdEqCoeffs("lds",mesh, FD, srcCoeffs, bdrVals)
-    aP, aW, aE, b = stdFormCoeffs
-    aPExpected = np.array([ 2.75,  1.0,   1.00,  1.00,  0.25])
-    aWExpected = np.array([ 0.00,  1.75,  1.75,  1.75,  1.75])
-    aEExpected = np.array([-0.75, -0.75, -0.75, -0.75,  0.00])
-    bExpected =  np.array([ 3.50,  0.00,  0.00,  0.00,  0.00])
-#    printReport("aP", aPExpected, aP)
-#    printReport("aW", aWExpected, aW)
-#    printReport("aE", aEExpected, aE)
-#    printReport("b",  bExpected, b)
-
-    printArray(aW, "aW:")
-    printArray(aE, "aE:")
-    printArray(aP, "aP:")
-    printArray(b,  "bP:")
-    printArray((np.abs(aE) + np.abs(aW))/np.abs(aP), "Sc:")
-    ylds = np.around(solve.solve(stdFormCoeffs),decimals=4)
-    yexpected = [1.0356, 0.8694, 1.2573, 0.3521, 2.4644]
+    aP, aW, aE, bP = stdFormCoeffs
+    aP_v = [1.55, 1.00, 1.00, 1.00, 1.45]
+    aW_v = [0.00, 0.55, 0.55, 0.55, 0.55]
+    aE_v = [0.45, 0.45, 0.45, 0.45, 0.00]
+    bP_v = [1.10, 0.00, 0.00, 0.00, 0.00]
+    passed = report_aPWEb((aP,aW,aE,bP), (aP_v,aW_v,aE_v,bP_v))
+    yc = np.around(solve.solve(stdFormCoeffs),decimals=4)
     xc, xf = mesh
-    f_ana = analytic.AnalyticSolution(0.0, 1.0, bdrVals, mu)
-    yanalytic = f_ana(xc)
-    print "  #      x        yc         ya"
-    for i,(x,y,ya) in enumerate(zip(xc,ylds,yanalytic)):
-        print ("{:2d}  "+"{:4f}  "*3).format(i+1, x, y, ya)
-    print "Error: ",f_ana.error(xc,ylds)
-    
+    fa = analytic.AnalyticSolution(0.0, 1.0, bdrVals, mu)
+    ya = fa(xc)
+    yv = [0.9421, 0.8006, 0.6276, 0.4163, 0.1579]
+    passed = passed and report_y(xc, yc, yv, ya)
+    return passed
 
 
-def test_uws():
-    mesh = meshmaker.ucmesh(25, 0.0, 1.0)
-    FD, srcCoeffs, bdrVals, mu = init(mesh)
+
+#5.1(ii): v=2.5, N=5, LDS
+def test_V51ii():
+    print "\n"+30*"="
+    print "V&M, Example 5.1(ii): LDS, N=5, v=2.5"
+    mesh = meshmaker.ucmesh(5, 0.0, 1.0)
+    FD, srcCoeffs, bdrVals, mu = init(mesh, velocity=2.5)
+    stdFormCoeffs = discr.stdEqCoeffs("lds",mesh, FD, srcCoeffs, bdrVals)
+    aP, aW, aE, bP = stdFormCoeffs
+    aP_v = [2.75, 1.00, 1.00, 1.00, 0.25]
+    aW_v = [0.00, 1.75, 1.75, 1.75, 1.75]
+    aE_v = [-0.75, -0.75, -0.75, -0.75, 0.00]
+    bP_v = [3.50, 0.00, 0.00, 0.00, 0.00]
+    passed = report_aPWEb((aP,aW,aE,bP), (aP_v,aW_v,aE_v,bP_v))
+    yc = np.around(solve.solve(stdFormCoeffs),decimals=4)
+    xc, xf = mesh
+    fa = analytic.AnalyticSolution(0.0, 1.0, bdrVals, mu)
+    ya = fa(xc)
+    yv = [1.0356, 0.8694, 1.2573, 0.3521, 2.4644]
+    passed = report_y(xc, yc, yv, ya) and passed
+    return passed
+
+
+#5.2(i): v=0.1, N=5, UWS
+def test_V52i():
+    print "\n"+30*"="
+    print "V&M, Example 5.2(i): UWS, N=5, v=0.1"
+    mesh = meshmaker.ucmesh(5, 0.0, 1.0)
+    FD, srcCoeffs, bdrVals, mu = init(mesh, velocity=0.1)
     stdFormCoeffs = discr.stdEqCoeffs("uws", mesh, FD, srcCoeffs, bdrVals)
-    aP, aW, aE, b = stdFormCoeffs
-    aPExpected = np.array([ 4.00,  3.50,  3.50,  3.50,  4.00])
-    aWExpected = np.array([ 0.00,  3.00,  3.00,  3.00,  3.00])
-    aEExpected = np.array([ 0.50,  0.50,  0.50,  0.50,  0.00])
-    bExpected =  np.array([ 3.50,  0.00,  0.00,  0.00,  0.00])
-#    printReport("aP", aPExpected, aP)
-#    printReport("aW", aWExpected, aW)
-#    printReport("aE", aEExpected, aE)
-#    printReport("b",  bExpected, b)
-
-    print "aW:",aW
-    print "aE:",aE
-    print "aP:",aP
-    print "bP:",b
-    print "Scarborough:", (np.abs(aE) + np.abs(aW))/np.abs(aP)
-    yuws = np.around(solve.solve(stdFormCoeffs),decimals=4)
-    yexpected = [0.9998, 0.9987, 0.9921, 0.9524, 0.7143]
+    aP, aW, aE, bP = stdFormCoeffs
+    aP_v = np.array([ 1.60,  1.10,  1.10,  1.10,  1.60])
+    aW_v = np.array([ 0.00,  0.60,  0.60,  0.60,  0.60])
+    aE_v = np.array([ 0.50,  0.50,  0.50,  0.50,  0.00])
+    bP_v = np.array([ 1.10,  0.00,  0.00,  0.00,  0.00])
+    passed = report_aPWEb((aP,aW,aE,bP), (aP_v,aW_v,aE_v,bP_v))
+    yc = np.around(solve.solve(stdFormCoeffs),decimals=4)
     xc, xf = mesh
-    f_ana = analytic.AnalyticSolution(0.0, 1.0, bdrVals, mu)
-    yanalytic = f_ana(xc)
-    print "  #      x        yc        ya"
-    for i,(x,y,ya) in enumerate(zip(xc,yuws,yanalytic)):
-        print ("{:2d}  "+"{:4f}  "*3).format(i+1, x, y, ya)
-    print "Error: ",f_ana.error(xc,yuws)
+    fa = analytic.AnalyticSolution(0.0, 1.0, bdrVals, mu)
+    ya = fa(xc)
+    yv = [0.9337, 0.7879, 0.6130, 0.4031, 0.1512]
+    passed = report_y(xc, yc, yv, ya) and passed
+    return passed
 
 
-def test_hyb():
-    mesh = meshmaker.ucmesh(25, 0.0, 1.0)
-    FD, srcCoeffs, bdrVals, mu = init(mesh)
+
+#5.2(ii): v=2.5, N=5, UWS
+def test_V52i():
+    print "\n"+30*"="
+    print "V&M, Example 5.2(ii): UWS, N=5, v=2.5"
+    mesh = meshmaker.ucmesh(5, 0.0, 1.0)
+    FD, srcCoeffs, bdrVals, mu = init(mesh, velocity=2.5)
+    stdFormCoeffs = discr.stdEqCoeffs("uws", mesh, FD, srcCoeffs, bdrVals)
+    aP, aW, aE, bP = stdFormCoeffs
+    aP_v = np.array([ 4.00,  3.50,  3.50,  3.50,  4.00])
+    aW_v = np.array([ 0.00,  3.00,  3.00,  3.00,  3.00])
+    aE_v = np.array([ 0.50,  0.50,  0.50,  0.50,  0.00])
+    bP_v = np.array([ 3.50,  0.00,  0.00,  0.00,  0.00])
+    passed = report_aPWEb((aP,aW,aE,bP), (aP_v,aW_v,aE_v,bP_v))
+    yc = np.around(solve.solve(stdFormCoeffs),decimals=4)
+    xc, xf = mesh
+    fa = analytic.AnalyticSolution(0.0, 1.0, bdrVals, mu)
+    ya = fa(xc)
+    yv = [0.9998, 0.9987, 0.9921, 0.9524, 0.7143]
+    passed = report_y(xc, yc, yv, ya) and passed
+    return passed
+
+
+
+#5.3: v=2.5, N=5, HYB
+def test_V53():
+    print "\n"+30*"="
+    print "V&M, Example 5.3: HYB, N=5, v=2.5"
+    mesh = meshmaker.ucmesh(5, 0.0, 1.0)
+    FD, srcCoeffs, bdrVals, mu = init(mesh, velocity=2.5)
     stdFormCoeffs = discr.stdEqCoeffs("hyb", mesh, FD, srcCoeffs, bdrVals)
-    aP, aW, aE, b = stdFormCoeffs
-    aPExpected = np.array([3.500, 2.500, 2.500, 2.500, 3.500])
-    aWExpected = np.array([0.000, 2.500, 2.500, 2.500, 2.500])
-    aEExpected = np.array(5*[0.000])
-    bExpected = np.array([3.500, 0.000, 0.000, 0.000, 0.000])
-#    printReport("aP", aPExpected, aP)
-#    printReport("aW", aWExpected, aW)
-#    printReport("aE", aEExpected, aE)
-#    printReport("b",  bExpected, b)
-
-    print "aW:",aW
-    print "aE:",aE
-    print "aP:",aP
-    print "bP:",b
-    yhyb = np.around(solve.solve(stdFormCoeffs),decimals=4)
-    yexpected = [1.000, 1.000, 1.000, 1.000,0.7143]
+    aP, aW, aE, bP = stdFormCoeffs
+    aP_v = np.array([ 3.50,  2.50,  2.50,  2.50,  3.50])
+    aW_v = np.array([ 0.00,  2.50,  2.50,  2.50,  2.50])
+    aE_v = np.array([ 0.00,  0.00,  0.00,  0.00,  0.00])
+    bP_v = np.array([ 3.50,  0.00,  0.00,  0.00,  0.00])
+    passed = report_aPWEb((aP,aW,aE,bP), (aP_v,aW_v,aE_v,bP_v))
+    yc = np.around(solve.solve(stdFormCoeffs),decimals=4)
     xc, xf = mesh
-    f_ana = analytic.AnalyticSolution(0.0, 1.0, bdrVals, mu)
-    yanalytic = f_ana(xc)
-    print "  #      x        yc        ya"
-    for i,(x,y,ya) in enumerate(zip(xc,yhyb,yanalytic)):
-        print ("{:3d}  "+"{:4f}  "*3).format(i+1, x, y, ya)
-    print "Error: ",f_ana.error(xc,yhyb)
+    fa = analytic.AnalyticSolution(0.0, 1.0, bdrVals, mu)
+    ya = fa(xc)
+    yv = [1.0000, 1.0000, 1.0000, 1.0000, 0.7143]
+    passed = report_y(xc, yc, yv, ya) and passed
+    return passed
+
+
 
 
 def test_cai():
@@ -199,17 +246,15 @@ def test_cai():
     print "Error: ",f_ana.error(xc,ycai)
 
 
+
+
 if __name__ == "__main__":
-    print "LDS:"
-    test_lds()
-    print
-    print "UWS:"
-    test_uws()
-    print
-    print "HYB:"
-    test_hyb()
-    print
-    print "CAI:"
-    test_cai()
-    print
+    passed = test_V51i()
+    print "Global result:", okIfTrue(passed)
+    passed = test_V51ii() and passed
+    print "Global result:", okIfTrue(passed)
+    passed = test_V52i() and passed
+    print "Global result:", okIfTrue(passed)
+    passed = test_V53() and passed
+    print "Global result:", okIfTrue(passed)
     
