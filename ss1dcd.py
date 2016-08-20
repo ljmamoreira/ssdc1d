d00109fdf61703b57016867e7745df34a6ef7b5d
Jose Amoreira <ljmamoreira@gmail.com>
2016-08-20 23:44:35 +0100

    Rewrote as func calls most of main. Convergence rate now computable.

--- ss1dcd.py
+++ ss1dcd.py
@@ -27,7 +27,7 @@ class GFVSolution(object):
         self.srcCoeffs = srcCoeffs
         self.bdrVals = bdrVals
         F,D = FD
-        self.xc, self.xf = mesh
+        xc, xf = mesh
         dx0 = xc[0] - xf[0]
         mu = F[0] / (D[0] * dx0)
         self.f_ana = aux.AnalyticSolution(0.0, 1.0, bdrVals, mu)
@@ -42,7 +42,8 @@ class GFVSolution(object):
 
     def error(self):
         self.compute()
-        return self.f_ana.error(self.xc, self.yfv)
+        xc = self.mesh[0]
+        return self.f_ana.error(xc, self.yfv)
 
 #CASSolution: CAS solution, iherits from GVFSolution
 class CASSolution(GFVSolution):
@@ -61,9 +62,73 @@ class CASSolution(GFVSolution):
 #Common interface to create FVSolution objects
 def fvsolution(mesh,scheme,FD,srcCoeffs,bdrVals,*args):
     if scheme == "cas":
+        xsi = args[0]
         return CASSolution(mesh,scheme,FD,srcCoeffs,bdrVals,xsi)
     else:
         return GFVSolution(mesh,scheme,FD,srcCoeffs, bdrVals)
+
+
+
+def best_xsi(mesh, physPars, bdrVals, srcCoeffs):
+    v, rho, gamma = physPars
+    best_xsis = []
+    for v in np.linspace(0.1, 5.0, 50):
+        physPars = v, rho, gamma
+        F = np.ones_like(xf) * rho * v
+        D = np.zeros_like(xf)
+        D[1:-1] = gamma / (xc[1:] - xc[:-1])
+        D[0] = gamma / (xc[0] - xf[0])
+        D[-1] = gamma / (xf[-1] - xc[-1])
+        FD = F,D
+        FVSys = fvsolution(mesh, "cas", FD, srcCoeffs, bdrVals, 0.5)
+        optimal = minimize(FVSys.eofxsi, 0.5)
+        best_xsis.append(optimal.x[0])
+        #print "{:4.1f}  {:7.4f}  {:11.4e}".format(v,optimal.x[0],optimal.fun)
+    bxsi = np.average(best_xsis)
+    return bxsi
+
+
+
+def convergence_rate(physPars, bdrVals, xsi):
+    v, rho, gamma = physPars
+    with open("conv_rate.dat", "w") as erfile:
+        for n in [5, 21, 81, 161, 321]:
+            dx = 1.0 / n
+            mesh = meshmaker.ucmesh(n, 0.0, 1.0)
+            xc, xf = mesh
+            F = np.ones_like(xf) * rho * v
+            D = np.zeros_like(xf)
+            D[1:-1] = gamma / (xc[1:] - xc[:-1])
+            D[0] = gamma / (xc[0] - xf[0])
+            D[-1] = gamma / (xf[-1] - xc[-1])
+            FD = (F,D)
+            M = np.zeros(n)
+            N = np.zeros(n)
+            srcCoeffs = (M,N)
+            line = "{:6.4f}  ".format(dx)
+            for scheme in ("lds", "uws", "hyb", "cas"):
+                FVsys = fvsolution(mesh,scheme,FD,srcCoeffs, bdrVals, xsi)
+                err = FVsys.error()
+                line += "{:6.4e}  ".format(err)
+            print line
+            erfile.write(line+'\n')
+
+
+def standardSol(n,scheme,xsi=0.5):
+    mesh = meshmaker.ucmesh(n, 0.0, 1.0)
+    v = 2.5; rho = 1.0; gamma = 0.1; 
+    xc,xf = mesh
+    F = np.ones(n+1) * v * rho
+    D = np.zeros(n+1); 
+    D[0] = gamma/(xc[0]-xf[0])
+    D[1:-1] = gamma/(xc[1:]-xc[:-1])
+    D[-1] = gamma/(xf[-1] - xc[-1])
+    FD = (F,D)
+    srcCoeffs = (np.zeros(n), np.zeros(n))
+    bdrVals = (1.0, 0.0)
+    return fvsolution(mesh, scheme, FD, srcCoeffs, bdrVals, xsi)
+
+
 
 
 
@@ -80,47 +145,21 @@ if __name__ == "__main__":
         var,val = (x.strip() for x in assignment.split("="))
         locals()[var] = type(locals()[var])(val)
 
+    physPars = (v, rho, gamma)
     bdrVals = (phib_W, phib_E)
 
     M = np.zeros(n)
     N = np.zeros(n)
     srcCoeffs = (M,N)
 
-
     mesh = meshmaker.ucmesh(n, 0.0, 1.0)
     xc, xf = mesh
+    
+    #Determination of best xsi in the velocity range [0:5]
+    xsi = best_xsi(mesh, physPars, bdrVals, srcCoeffs)
+    print "best_xsi:",xsi
 
-    bxsi = []
-    print ("{:^7s}"+4*"{:^12s}"+"{:^6s}").format("v","lds","uws","hyb","cas", "bxsi")
-    for v in (0.1*i for i in range(1,41)):
-        line = "{:4.2f}   ".format(v)
-        physPars = (v, rho, gamma)
-        F = np.ones_like(xf) * rho * v
-        D = np.zeros_like(xf)
-        D[1:-1] = gamma / (xc[1:] - xc[:-1])
-        D[0] = gamma / (xc[0] - xf[0])
-        D[-1] = gamma / (xf[-1] - xc[-1])
-        FD = (F,D)
-        for scheme in ("lds", "uws", "hyb", "cas"):
-            FVsys = fvsolution(mesh,scheme,FD,srcCoeffs, bdrVals, xsi)
-
-            if scheme == "cas":
-                optimal = minimize(FVsys.eofxsi, 0.5)
-                err = optimal.fun
-                best_xsi = optimal.x[0]
-                bxsi.append(best_xsi)
-                line += "{:6.4e}  {:6.4f}".format(err,best_xsi)
-            else:
-                err = FVsys.error()
-                line += "{:6.4e}  ".format(err)
-        print line
-
-    print
-    bxsi = np.array(bxsi)
-    best_xsi = np.average(bxsi)
-    print "Average xsi:", best_xsi
-    print "Std xsi:    ", np.std(bxsi)
-
+    #Compare errors
     with open('errtab.dat','w') as ofile:
         print ("{:^7s}"+4*"{:^12s}").format("v","lds","uws","hyb","cas")
         for v in (0.1*i for i in range(1,41)):
@@ -133,10 +172,11 @@ if __name__ == "__main__":
             D[-1] = gamma / (xf[-1] - xc[-1])
             FD = (F,D)
             for scheme in ("lds", "uws", "hyb", "cas"):
-                FVsys = fvsolution(mesh,scheme,FD,srcCoeffs, bdrVals, best_xsi)
+                FVsys = fvsolution(mesh,scheme,FD,srcCoeffs, bdrVals, xsi)
                 err = FVsys.error()
                 line += "{:6.4e}  ".format(err)
             ofile.write(line+'\n')
             print line
 
-
+    v = 2.5; rho = 1.0; gamma = 0.1; physPars = (v, rho, gamma)
+    convergence_rate(physPars, bdrVals, xsi)
